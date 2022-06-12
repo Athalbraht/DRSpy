509dfd3c7a38662586408922237ffa0f718e1732
aszadzinski <albert.szadzinski@us.edu.pl>
2022-06-12 09:12:32 +0200

    Updated docstrings in InSpector class

--- DRSpy/scripts/inspector1000.py
+++ DRSpy/scripts/inspector1000.py
@@ -11,6 +11,10 @@ from DRSpy.data_struct import log
 
 class InSpector():
     """
+    Spectrum analyzer for InSpector1000. 
+
+    :param plot_ext: extension for generated plots, defaults to png
+    :type plot_ext: str
     """
     def __init__(self, plot_ext="png"):
         log("--> Initialize InSpector-100 module")
@@ -18,7 +22,16 @@ class InSpector():
         self.calibration = 1
     
     def calibrate(self, calibfile):
-        """"""
+        """
+        Calibration method. Create linear corelation between peaks in .tka and real decay energy.
+        Example of input calibration file example.txt:
+        Channel,E,Source
+        102,511,Na22
+        251,1274,Na22
+
+        :param calibfile: input calib .csv
+        :type calibfile: str
+        """
         log("---> Starting calibration: ", wait=True)
         fig, ax = create_figure()
         try:
@@ -38,14 +51,23 @@ class InSpector():
             log("--> Calculated a param: ", wait=True); log(f"a = {round(p[0],3)} ± {round(pc[0,0], 3)} ", "green")
             self.calibration = p[0]
     
-    def get_spectrum(self, paths, normalize=False):
-        """ """
+    def get_spectrum(self, paths, normalize=False, calibration=True):
+        """
+        Get spectrum from TKA files.
+
+        :param paths: List of paths to tka files
+        :type paths: list(str)
+        :param normalize: Normalize generated spectrum, avalible methods: minmax, defaults to False
+        :type normalize: str
+        :param calibration: enable or disable calibration, defaults to True
+        :type calibration: bool
+        """
         _ylabel = "Counts"
         if normalize: _ylabel += " [MinMax Norm]"
-        if self.calibration == 1:
+        if self.calibration == 1 or calibration:
             log("---> Missing calibration", "yellow")
             _xlabel = "Channel"
-            self.calib_scaler = self.calibration
+            self.calib_scaler = 1
         else:
             _xlabel = "Energy [MeV]"
             self.calib_scaler = self.calibration * 1e-3
@@ -70,7 +92,17 @@ class InSpector():
             log(f"\n{e}")
 
     def normalize(self, data, method="minmax"):
-        """ """
+        """
+        Normalize dataset.
+
+        :param data: 1D list to normalize
+        :type data: numpy.ndarray
+        :param method: method of normalization, defaults to minmax, available: minmax, propability
+        :type method: str
+
+        :return: normalized list of data
+        :rtype: numpy.ndarray
+        """
         data = np.array(data)
         normalized_data = np.zeros_like(data)
         for n,nw in enumerate(data):
@@ -78,20 +110,3 @@ class InSpector():
                 normalized_data[n] = (nw-data.min())/(data.max()-data.min())
         return normalized_data
             
-       
-
-if __name__  == "__main__":
-    files = argv[1::]
-    filenames = [i.split(".")[0] for i in files]
-    
-    for n, file in enumerate(files):
-        with open(file, "r") as ff:
-            _dat = [float(i) for i in ff.readlines()][2::]
-            Y = np.array(_dat)
-            X = np.arange(0, len(Y))
-            plt.plot(X,Y, label=filenames[n])
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    
-
